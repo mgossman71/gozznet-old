0b8eb3b878f5eac5e9da75a1a402a4a8fa4be4aa
Mark Gossman <gozz@Marks-MacBook-Air-M1.local>
2022-12-15 06:18:24 -0600

    update

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-version=v14
+version=v15
 
 docker build . -t mgossman71/gozznet-old:$version
 docker build . -t mgossman71/gozznet-old:latest
 
-# docker push mgossman71/gozznet-old:$version
-# docker push mgossman71/gozznet-old:latest
+docker push mgossman71/gozznet-old:$version
+docker push mgossman71/gozznet-old:latest
 
 # kubectl create deployment gozznet-old  --image=local/gozznet-old --replicas=3
 # kubectl expose deployment gozznet-old --type=NodePort --name=gozznet-old-svc
